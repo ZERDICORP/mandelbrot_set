d1145505041f7db146708aded09221a89c1eea8a
ZERDICORP <nikolaianikin2002@gmail.com>
2021-07-17 23:13:27 +0300

    error correction

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ REM ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑�
 SET includePath=%sfmlPath%/include
 SET libPath=%sfmlPath%/lib
 
-g++ ./src/implementation/*.cpp -o ./build/main.exe -O3 -O2 -O1 -DSFML_STATIC -I"C:/Program Files/SFML-2.5.1/include" -I"./src/headers" -L"C:/Program Files/SFML-2.5.1/lib" -lsfml-graphics -lsfml-window -lsfml-system
+g++ ./src/implementation/*.cpp -o ./build/main.exe -O3 -O2 -O1 -DSFML_STATIC -I %includePath% -I"./src/headers" -L %libPath% -lsfml-graphics -lsfml-window -lsfml-system
 
 echo Press any button..
 pause > nul
